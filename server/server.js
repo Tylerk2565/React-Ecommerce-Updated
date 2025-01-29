f20f526680c53f84a2c8ce76d32d49eaf3e506b9
Tylerk2565 <ty25566@gmail.com>
2025-01-29 12:24:49 -0500

    set link for cors

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 // Since we have 2 different ports, this is how we connect those ports to fetch our data
 // Cors helps us do that securely
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["https://react-ecommerce-frontend-qi5q.onrender.com/"],
 };
 
 app.use(cors(corsOptions));
